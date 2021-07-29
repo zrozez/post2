c1686e30ab6236b83d54cd226d00087097d893cd
zrozez <r.sakhizova@gmail.com>
2021-07-29 20:51:35 +0600

    Add user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.forms.fields import CharField
-from django.forms.widgets import TextInput
+from django.contrib.auth.models import AbstractUser
 
 
-# class User(models.Model):
-#     username = CharField(max_length=24)
-#     password = CharField(max_length=50)
-#     first_name = TextInput()
-#     last_name = TextInput()
+class User(AbstractUser):
+
+    class UserType(models.TextChoices):
+        ADMIN = 'admin'
+        EDITOR = 'editor'
+        ORDINARY = 'ordinary'
+
+    role = models.CharField(max_length=10, choices=UserType.choices, default=UserType.ORDINARY)
 
