6576af90faec7627daf8974cac7db33654e3e16f
zrozez <r.sakhizova@gmail.com>
2021-07-29 20:56:11 +0600

    Set up admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        ('System credentials', {'fields': ('username', 'password')}),
+        ('Permissions', {'fields':('role', )})
+    )
+
+admin.site.register(User, UserAdmin)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'body', ]
+
+
+admin.site.register(Post, PostAdmin)
